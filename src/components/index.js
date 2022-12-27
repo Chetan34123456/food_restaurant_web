b94e3e101a93412b6291f6696a4f3d828f5d4205
chetan <chetanhiwale67@gmail.com>
2022-12-28 01:33:03 +0530

    initial commit

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import SubHeading from './SubHeading/SubHeading';
+import Newsletter from './footer/Newsletter';
+import FooterOverlay from './footer/footerOverlay';
+import MenuItem from './MenuItem/MenuItem';
+import Navbar from './navbar/navbar';
+
+export {
+  SubHeading,
+  Newsletter,
+  FooterOverlay,
+  MenuItem,
+  Navbar,
+};
